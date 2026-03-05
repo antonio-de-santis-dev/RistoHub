62cf64cc54f4be3496dfcb7ad71fc55cc9304bf7
antonio-de-santis-dev <adesantis@studenti.apuliadigital.it>
2026-03-05 11:58:47 +0100

    futer ora funzionante e adattato

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslateDirective } from 'app/shared/language';
 
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
-  imports: [],
+  imports: [TranslateDirective],
 })
 export default class FooterComponent {
   currentYear = new Date().getFullYear();
--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Renderer2, RendererFactory2, inject } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Component, OnInit, Renderer2, RendererFactory2, inject, signal } from '@angular/core';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
 import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { filter } from 'rxjs/operators';
 import dayjs from 'dayjs/esm';
 
 import { AccountService } from 'app/core/auth/account.service';
@@ -8,6 +9,9 @@ import { AppPageTitleStrategy } from 'app/app-page-title-strategy';
 import FooterComponent from '../footer/footer.component';
 import PageRibbonComponent from '../profiles/page-ribbon.component';
 import { LoaderComponent } from 'app/shared/loader/loader.component';
+
+// Rotte su cui il footer NON deve apparire
+const ROUTES_WITHOUT_FOOTER = ['/menu-view', '/menu-public'];
 
 @Component({
   selector: 'jhi-main',
@@ -17,6 +21,8 @@ import { LoaderComponent } from 'app/shared/loader/loader.component';
 })
 export default class MainComponent implements OnInit {
   private readonly renderer: Renderer2;
+
+  showFooter = signal(true);
 
   private readonly router = inject(Router);
   private readonly appPageTitleStrategy = inject(AppPageTitleStrategy);
@@ -31,6 +37,12 @@ export default class MainComponent implements OnInit {
   ngOnInit(): void {
     // try to log in automatically
     this.accountService.identity().subscribe();
+
+    // Nascondi footer sulle rotte specificate
+    this.router.events.pipe(filter(e => e instanceof NavigationEnd)).subscribe((e: NavigationEnd) => {
+      const hide = ROUTES_WITHOUT_FOOTER.some(r => e.urlAfterRedirects.startsWith(r));
+      this.showFooter.set(!hide);
+    });
 
     this.translateService.onLangChange.subscribe((langChangeEvent: LangChangeEvent) => {
       this.appPageTitleStrategy.updateTitle(this.router.routerState.snapshot);
