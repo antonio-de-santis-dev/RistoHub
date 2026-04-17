bd9765f31753a821e28edd3d414f69f654717667
antonio-de-santis-dev <adesantis@studenti.apuliadigital.it>
2026-04-17 09:29:02 +0200

    implementazione aggiiuta lista prodotti con pdf funzioante

--- src/main/java/main/config/SecurityConfiguration.java
+++ src/main/java/main/config/SecurityConfiguration.java
@@ -78,6 +78,7 @@ public class SecurityConfiguration {
                     // ── Risorse statiche Angular ───────────────────────────────────────────
                     .requestMatchers(mvc.pattern("/index.html"), mvc.pattern("/*.js"), mvc.pattern("/*.txt"), mvc.pattern("/*.json"), mvc.pattern("/*.map"), mvc.pattern("/*.css")).permitAll()
                     .requestMatchers(mvc.pattern("/*.ico"), mvc.pattern("/*.png"), mvc.pattern("/*.svg"), mvc.pattern("/*.webapp")).permitAll()
+                    .requestMatchers(antMatcher("/*.docx")).permitAll()
                     .requestMatchers(mvc.pattern("/app/**")).permitAll()
                     .requestMatchers(mvc.pattern("/i18n/**")).permitAll()
                     .requestMatchers(mvc.pattern("/content/**")).permitAll()
